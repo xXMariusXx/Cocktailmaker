fb222cfb5c6d79080b280a97f5b695568703b77b
xLorena <41506057+xLorena@users.noreply.github.com>
2018-07-21 21:03:35 +0200

    add search bar

---
+++ ionic-app/src/pages/rezept-liste/rezept-liste.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavParams } from 'ionic-angular';
+
+
+/**
+ * Generated class for the LorenaActionSheetPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+@Component({
+  selector: 'page-lorena-action-sheet',
+  templateUrl: 'rezept-liste.component.html',
+
+})
+export class RezeptListe {
+
+  @Input()
+  constructor(private navParams: NavParams) {
+    var zutaten = navParams.get("zutaten");
+  }
+
+  }
+
+
