97c1c5a6f4867c476cdebc98e9d892e25c496b3d
Marius <41506006+xXMariusXx@users.noreply.github.com>
2018-07-21 17:00:48 +0200

    added mysql

---
+++ server/src/main/java/hello/MySQLAccess.java
@@ -0,0 +1,62 @@
+package hello;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import java.util.Properties;
+
+public class MySQLAccess {
+
+    // init database constants
+    private static final String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DATABASE_URL = "jdbc:mysql://51.255.49.81:3306/mysqlDoHack18";
+    private static final String USERNAME = "mysqlDoHack18";
+    private static final String PASSWORD = "myPWDoHack18";
+    //private static final String MAX_POOL = "250";
+
+    // init connection object
+    private Connection connection;
+    // init properties object
+    private Properties properties;
+
+    // create properties
+    private Properties getProperties() {
+        if (properties == null) {
+            properties = new Properties();
+            properties.setProperty("user", USERNAME);
+            properties.setProperty("password", PASSWORD);
+            //properties.setProperty("MaxPooledStatements", MAX_POOL);
+        }
+        return properties;
+    }
+
+
+    // connect database
+    public Connection connect() {
+        if (connection == null) {
+            try {
+                Class.forName(DATABASE_DRIVER);
+                connection = DriverManager.getConnection(DATABASE_URL, getProperties());
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    // disconnect database
+    public void disconnect() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
