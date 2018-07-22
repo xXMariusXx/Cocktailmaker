ec516ccfb78ad74da08567b5fac682a06189db2b
fridtjof <2780577+fridtjof@users.noreply.github.com>
2018-07-22 03:20:14 +0200

    oops

---
+++ server/src/main/java/getraenkeServer/ZutatenController.java
@@ -0,0 +1,17 @@
+package getraenkeServer;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@CrossOrigin("*")
+public class ZutatenController {
+    @RequestMapping("/zutaten")
+    public ArrayList zutaten() {
+        Controller controller = new Controller();
+        return controller.gibZutatenliste();
+    }
+}
